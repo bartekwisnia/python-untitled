4912b6ac921de43dc05cc9c58abafed747c46c15
bartekwisnia <bartekwisnia@op.pl>
2017-06-27 22:58:25 +0200

    Newest commit from Pycharm

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#/usr/bin/python3
+# /usr/bin/python3
+
 
 def main(msg):
-    	//Adding a different comment
-	// Print a message
+    # Another comment
+    # Adding a different comment
+    # Print a message
     print(msg)
     
 main("Hello people")
